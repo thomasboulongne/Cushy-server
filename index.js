35f9150961103cfc6a4eaa8ca2be78f255e520cf
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-01-06 15:19:04 +0100

    Git init

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var ctf = require('./data/contenful.js');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+app.listen(3637);
