6fa5bea1d6a9fec5e41163d1022ca52f5e4609cb
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-01-06 17:10:20 +0100

    Add contentful

--- data/contenful.js
+++ data/contenful.js
@@ -1,26 +1,15 @@
 var contentful = require('contentful-management')
 var client = contentful.createClient({
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: 'YOUR_ACCESS_TOKEN'
+  accessToken: '91cbfefc8741ae0809bc04a791e280912a54c254f80245b665b110f51a4b1c42'
 })
 
 // This API call will request a space with the specified ID
-client.getSpace('spaceId')
+client.getSpace('7dyud8u3k188')
 .then((space) => {
   // Now that we have a space, we can get entries from that space
   space.getEntries()
   .then((entries) => {
     console.log(entries.items)
-  })
-
-  // let's get a content type
-  space.getContentType('product')
-  .then((contentType) => {
-    // and now let's update its name
-    contentType.name = 'New Product'
-    contentType.update()
-    .then((updatedContentType) => {
-      console.log('Update was successful')
-    })
   })
 })
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express');
-// var ctf = require('./data/contenful.js');
-
-console.log('Hello its me adele ');
+var ctf = require('./data/contenful.js');
 
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
-    console.log(`hey adele it's me from the other side`)
+    console.log(`hey adele it's me from the other side`);
     res.end('Vous êtes à l\'accueil');
+});
+
+app.get('/sensor/:id/:data', (req, res) => {
+	let id = req.params.id;
+	let data = req.params.data;
+	
+	res.end(data);
 });
 
 app.listen(3637);
