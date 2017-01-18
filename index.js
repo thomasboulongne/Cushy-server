9a03da08867d7cf0d941423ac458369f5a05f90f
Thomas Boulongne <thomas@spill.net>
2017-01-18 17:50:40 +0100

    Remove contentful, add lowdb 〰️

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 'use strict';
-
 const express = require('express');
-const ctf = require('./data/contenful.js');
+const low = require('lowdb');
 
-const app = express();
+const db = low('./data/data.json');
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    console.log(`hey adele it's me from the other side`);
-    res.end('Vous êtes à l\'accueil');
+ctf.getSpace()
+.then( data => {
+	const space = data	
+
+	const app = express();
+
+	app.get('/', (req, res) => {
+	    res.setHeader('Content-Type', 'text/plain');
+	    console.log(`hey adele it's me from the other side`);
+	    res.end('Vous êtes à l\'accueil');
+	});
+
+	app.get('/sensor/:id/:data', (req, res) => {
+		let coussflix_id = req.params.id;
+		let data = req.params.data;
+
+		db.get('data')
+		.insert({
+			user_id: coussflix_id,
+			data: data,
+			timestamp: Date.now()
+		})
+		.value();
+		
+		res.end(data);
+
+	});
+
+	app.listen(3637);
 });
-
-app.get('/sensor/:id/:data', (req, res) => {
-	let id = req.params.id;
-	let data = req.params.data;
-
-	res.end(data);
-});
-
-app.listen(3637);
