1e0d35845643966f26593c2f2d0205e38540bbe0
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-01-25 14:18:18 +0100

    Allow cross domain/origin

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 
 const MovieManager = require('./classes/MovieManager');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/sensor/:id/:data', (req, res) => {
 	let coussflix_id = req.params.id;
 	let data = req.params.data;
