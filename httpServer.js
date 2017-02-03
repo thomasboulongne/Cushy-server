78bde6bb214c2883fc1ece2b4e0d25fd7959b3c1
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-02-03 11:24:32 +0100

    Allow cors on proxy

--- httpServer.js
+++ httpServer.js
@@ -5,6 +5,12 @@ const app = express();
 
 const request = require('request');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/sensor/:id/:data', (req, res) => {
 	let coussflix_id = req.params.id;
 	let data = req.params.data;
